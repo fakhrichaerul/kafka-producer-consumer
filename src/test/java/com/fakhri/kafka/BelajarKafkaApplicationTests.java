36d03b0f64657689cba91cd5c99180592695680d
fakhrichaerul <fakhrichaerul@gmail.com>
2021-04-05 10:15:01 +0700

    commit pertama

---
+++ src/test/java/com/fakhri/kafka/BelajarKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fakhri.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
